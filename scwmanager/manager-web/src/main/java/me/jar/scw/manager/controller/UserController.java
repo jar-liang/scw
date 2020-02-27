0665fb9e9efc5ccb7b40264faa893ac48d465f76
jar <ljyan0304@163.com>
2020-02-28 00:12:54 +0800

    打通前后端

---
+++ scwmanager/manager-web/src/main/java/me/jar/scw/manager/controller/UserController.java
@@ -0,0 +1,37 @@
+package me.jar.scw.manager.controller;
+
+import me.jar.scw.manager.model.TUser;
+import me.jar.scw.manager.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Description 用户登录注册控制器
+ * @Date 2020/2/27-23:05
+ */
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+    @Autowired
+    private IUserService userService;
+
+    /**
+     *  处理用户注册
+     * @param user
+     * @return
+     */
+    @RequestMapping("reg.do")
+    @ResponseBody
+    public String userReg(TUser user) {
+        System.out.println("UserController.userReg()");
+        Integer row = userService.createUser(user);
+        if (row == 1) {
+            return "{\"state\":\"success\"}";
+        } else {
+            return "{\"state\":\"fail\"}";
+        }
+    }
+}
