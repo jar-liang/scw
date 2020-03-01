6079c7e7f9c65d6f09b0ef6387ea31cbd137983e
jar <ljyan0304@163.com>
2020-03-01 12:09:32 +0800

    reg页面添加了表单校验

---
+++ scwmanager/manager-service/src/test/java/me/jar/scw/service/user/UserTest.java
@@ -0,0 +1,30 @@
+package me.jar.scw.service.user;
+
+import me.jar.scw.manager.model.TUser;
+import me.jar.scw.manager.service.IUserService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+/**
+ * @Description
+ * @Date 2020/3/1-9:55
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:springmvc4test.xml", "classpath:mybatisconfig.xml"})
+public class UserTest {
+    @Autowired
+    IUserService userService;
+
+    @Test
+    public void testFindUsers() {
+        List<TUser> users = userService.findAllUsers();
+        for (TUser user : users) {
+            System.out.println(user);
+        }
+    }
+}
