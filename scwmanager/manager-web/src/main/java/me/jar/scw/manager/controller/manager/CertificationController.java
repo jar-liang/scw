d71d91389c6199557887eba8c23c4e4a8ad942d2
jar <ljyan0304@163.com>
2020-07-07 00:03:11 +0800

    许可维护初步刷出权限树

---
+++ scwmanager/manager-web/src/main/java/me/jar/scw/manager/controller/manager/CertificationController.java
@@ -0,0 +1,14 @@
+package me.jar.scw.manager.controller.manager;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Description 资质维护controller
+ * @Date 2020/6/29-23:11
+ */
+@Controller
+@RequestMapping("cert")
+public class CertificationController {
+
+}
